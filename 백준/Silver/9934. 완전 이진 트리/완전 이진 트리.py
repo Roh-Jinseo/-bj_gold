520992c52348bdc7555a5cb88adddb1435936a9d
Roh-Jinseo <156387889+Roh-Jinseo@users.noreply.github.com>
2024-03-27 00:14:26 +0900

    [Silver I] Title: 완전 이진 트리, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
@@ -1,16 +1,11 @@
-from collections import deque
-def inorder(lev, idx):
-    if lev == K:
-        tree[idx] = arr.popleft()
-        return
-
-    inorder(lev+1,idx*2) #L
-    tree[idx] = arr.popleft()
-    inorder(lev+1,idx*2+1)#R
+def preorder(lev, s,e):
+    if lev == K:        return
+    tree[lev].append(arr[(s+e)//2])#V
+    preorder(lev+1,s, (s+e)//2-1) #L
+    preorder(lev+1,(s+e)//2+1, e)#R
 
 K = int(input())
-arr = deque(list(map(int, input().split())))
-tree = [0] + [0]*(2**K-1)
-inorder(1, 1)
-for i in range(K):    print(*tree[2**i:2**(i+1)])
-
+arr = list(map(int, input().split()))
+tree = [ [] for _ in range(K) ]
+preorder(0,0, len(arr))
+for t in tree:    print(*t)
